8f009184a6828ebb395c289a6f63fbf661aa11ff
odon-airoldi <odon.airoldi@gmail.com>
2026-02-20 15:47:08 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,24 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// for loop
+for(let i = 0; i < names.length; i++) {
+
+    console.log(names[i])
+
+}
+
+// forEach loop
+names.forEach(function(name, index, arr) {
+
+    // console.log(name, index, arr);
+    console.log(name)
+
+})
+
+
+
+ 
